2fab8fca18fbd1112167c2f4c97e5946dff3ef40
Roshan <roshan.roshan@hotmail.com>
2025-05-08 09:27:47 -0600

    commit-for-may-7

--- personalfinance/urls.py
+++ personalfinance/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('myfinance.urls')),
+    path('',include('index.urls')),
     path('accounts/',include('django.contrib.auth.urls')),
     path('users/',include('users.urls')),
 ]
